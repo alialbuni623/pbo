db38f2c4250ca058bbe5a74f9f4120f549fe3eeb
alialbuni623 <alialbuni623@gmail.com>
2025-02-02 18:54:08 +0700

    uas-ali-albuni

--- UAS/UAS_ALI_ALBUNI/health/tests.py
+++ UAS/UAS_ALI_ALBUNI/health/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from .models import Activity, FoodIntake, VitalSigns
+from django.urls import reverse
 
-# Create your tests here.
+class ActivityModelTest(TestCase):
+    def setUp(self):
+        self.activity = Activity.objects.create(
+            user_id=1,
+            date='2023-01-01',
+            steps=1000,
+            distance=1.5,
+            calories_burned=100
+        )
+
+    def test_activity_creation(self):
+        self.assertEqual(self.activity.steps, 1000)
+
+class FoodIntakeModelTest(TestCase):
+    def setUp(self):
+        self.food_intake = FoodIntake.objects.create(
+            user_id=1,
+            date='2023-01-01',
+            food_item='Apple',
+            calories=95,
+            nutrients={'carbs': 25, 'protein': 0.5}
+        )
+
+    def test_food_intake_creation(self):
+        self.assertEqual(self.food_intake.food_item, 'Apple')
+
+class VitalSignsModelTest(TestCase):
+    def setUp(self):
+        self.vital_signs = VitalSigns.objects.create(
+            user_id=1,
+            date='2023-01-01',
+            blood_pressure='120/80',
+            weight=70.0,
+            blood_sugar=90.0
+        )
+
+    def test_vital_signs_creation(self):
+        self.assertEqual(self.vital_signs.blood_pressure, '120/80')
+
+class IndexViewTest(TestCase):
+    def test_index_view(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'health/index.html')
